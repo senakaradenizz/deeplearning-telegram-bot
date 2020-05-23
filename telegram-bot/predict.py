d5e125ff9f12b6a8d0eaf06dfbe4612e9654e2bf
senakaradenizz <senaakaradenizz@gmail.com>
2020-05-23 12:15:14 +0300

    bug fixed

--- telegram-bot/predict.py
+++ telegram-bot/predict.py
@@ -10,6 +10,7 @@ import pickle
 import tensorflow as tf
 global graph, sess
 import nlp
+import matplotlib.pyplot as plt
 
 
 graph = tf.Graph()
@@ -27,16 +28,42 @@ try:
             model = load_model('model.keras')  
     
 except:    
+    
     model = Sequential()
     model.add(Dense(16,input_shape=(len(training[0]),),activation="relu"))
     model.add(Dense(16,activation="relu"))
     model.add(Dropout(0.3))
     model.add(Dense(10,activation="softmax"))
     model.summary()
-    model.compile(Adam(lr=.001),loss="categorical_crossentropy",metrics=["accuracy"])
-    model.fit(training, output,epochs=200, verbose=2,batch_size=4)
+    model.compile(Adam(lr=.001),loss="categorical_crossentropy",metrics=["acc"])
+    history = model.fit(training, output, validation_split = 0.1, epochs=200, verbose=1,batch_size=4)
     model.save("model.keras")
+    
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
 
+
+epochs = range(1,len(acc)+1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('epochs')
+plt.ylabel('accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.legend()
+
+plt.show()
 
 def prediction(x):
     with graph.as_default():
